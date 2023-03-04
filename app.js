e2593976df19f2fdadb676f742f1542ce5629ee5
Carlos-Gabriel-Rojas <carlosgabrielrojas.dev@gmail.com>
2023-03-04 16:10:27 -0300

    DOM elements variables

---
+++ app.js
@@ -0,0 +1,7 @@
+// DOM elements
+const form = document.querySelector("#search-form");
+const input = document.querySelector("#search-term");
+const msg = document.querySelector(".form-msg");
+const list = document.querySelector(".cities");
+
+const apiKey = "4d8fb5b93d4af21d66a2948710284366";
