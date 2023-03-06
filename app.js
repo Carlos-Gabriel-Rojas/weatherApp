d4d02312fe984684e4aebb774b4531c56607ccef
Carlos-Gabriel-Rojas <carlosgabrielrojas.dev@gmail.com>
2023-03-06 09:42:29 -0300

    fetching weather data from openweathermap

--- app.js
+++ app.js
@@ -59,3 +59,15 @@ function getExistingCity(searchValue) {
       }
     });
 }
+
+async function getWeatherData(city) {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    const response = await fetch(url);
+    const data = await response.json();
+  
+    if (response.ok) {
+      return data;
+    } else {
+      throw new Error(`${data.cod}, ${data.message}`);
+    }
+  }
